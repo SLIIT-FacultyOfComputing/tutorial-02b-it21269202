47d62c204e4d89be17c489d6544dc0925e0a2251
Tharushi Kodithuwakku <99242653+it21269202@users.noreply.github.com>
2022-02-16 06:12:28 +0000

    Tutorial 2 - I did all four activities

--- tute02.cpp
+++ tute02.cpp
@@ -12,7 +12,7 @@ Type = 3
 OtRate = 1700
 Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
 
-#include <stdio.h>
+/*#include <stdio.h>
 int main()
 {
    double salary, netSalary;
@@ -41,4 +41,32 @@ int main()
    printf("Net Salary is %f ", netSalary);
   
    return 0;
+}*/
+
+#include<iostream>
+int main()
+{
+  double salary, netSalary;
+  int eType, otHrs, otRate;
+  std::cout << "Enter Employee Type : ";
+  std::cin >> eType;
+  std::cout << "Enter Salary : ";
+  std::cin >> salary;
+  std::cout << "Enter OtHrs : ";
+  std::cin >> otHrs;
+
+  switch(eType){
+    case 1 :
+        otRate = 1000;
+        break;
+    case 2 :
+        otRate = 1500;
+        break;
+    default :
+        otRate = 1700;
+        break;
+  }
+  netSalary = salary + otHrs* otRate;
+  std::cout << "Net Salary : "<<netSalary;
+  return 0;
 }
--- tute04.cpp
+++ tute04.cpp
@@ -12,18 +12,31 @@ long nCr(int n, int r);
 Do not modify the main function.*/
 
 #include <iostream>
-
 long Factorial(int no);
 long nCr(int n, int r);
 
 int main() {
-  int n, r;
-  std::cout << "Enter a value for n ";
-  std::cin >> n;
-  std::cout << "Enter a value for r ";
-  std::cin >> r;
-  std::cout << "nCr = ";
-  std::cout << nCr(n,r);
-  std::cout << std::endl;
-  return 0;
+ int n, r;
+ std::cout << "Enter a value for n ";
+ std::cin >> n;
+ 
+ std::cout << "Enter a value for r ";
+ std::cin >> r;
+ 
+ std::cout << "nCr = ";
+ std::cout << nCr(n,r);
+ std::cout << std::endl;
+}
+long nCr(int n, int r)
+{
+	return Factorial(n)/(Factorial(r)*Factorial(n-r));
+}
+long Factorial(int no)
+{
+	int x,fac=1;
+	for(x=1;x<=no;x++)
+	{
+		fac=fac*x;
+	}
+	return fac;
 }
